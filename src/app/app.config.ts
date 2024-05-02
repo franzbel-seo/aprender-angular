6f0d04059951dd1f4ba57f4e57cb60ca35aee7ac
Franz Beltran <fbeltran@ucb.edu.bo>
2024-05-02 17:31:52 -0400

    Incluir interceptores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {authInterceptor} from "./interceptors/auth.interceptor";
+import {logginInterceptor} from "./interceptors/loggin.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient( withInterceptors([authInterceptor, logginInterceptor]))
   ]
 };
